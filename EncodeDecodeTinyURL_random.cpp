1bd403b1e9a4adfaa3eae52be3d8548a346e902d
Guangchu Shi <rollschild@gmail.com>
2017-09-15 12:21:07 -0400

    Create EncodeDecodeTinyURL_random.cpp

---
+++ EncodeDecodeTinyURL_random.cpp
@@ -0,0 +1,43 @@
+class Solution {
+    public:
+    
+    string dictionary = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    
+    map<string, string> URLmap;
+    string header = "http://tinyurl.com/";
+    int len = header.length();
+    string resolution = "";
+    
+    string getRandomString() {
+        string randString = "";
+        default_random_engine generator;
+        uniform_int_distribution<int> distribution(0, 61);
+        for(int i = 0; i < 6; ++i) {
+            randString += dictionary[distribution(generator)];    
+        }
+        return randString;
+    }
+    
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) {       
+        resolution = getRandomString();
+        if(URLmap.find(resolution) != URLmap.end()) return URLmap[resolution];
+        else {
+            URLmap[resolution] = longUrl;
+            return header + resolution;         
+        }
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        shortUrl.replace(0, len - 1, "");
+        if(URLmap.find(shortUrl) != URLmap.end()) {
+            return URLmap[shortUrl];
+        }
+        else return "";
+    }
+};
+
+// Your Solution object will be instantiated and called as such:
+// Solution solution;
+// solution.decode(solution.encode(url));
