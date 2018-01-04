bb7b8ed8bbbe5519bb83f897a1bd347f123f5f77
Guangchu Shi <rollschild@localhost.localdomain>
2018-01-03 21:52:59 -0500

    Initial commit

--- EncodeDecodeTinyURL_random.cpp
+++ EncodeDecodeTinyURL_random.cpp
@@ -1,3 +1,4 @@
+#include <random>
 class Solution {
     public:
     
--- HackerRank/HackerRank/main.cpp
+++ HackerRank/HackerRank/main.cpp
@@ -1,15 +1,36 @@
-//
-//  main.cpp
-//  HackerRank
-//
-//  Created by Guangchu Shi on 10/5/17.
-//  Copyright © 2017 Guangchu Shi. All rights reserved.
-//
-
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
+#include <algorithm>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n, q;
+    int lastAnswer = 0;
+    cin >> n >> q;
+    vector<vector<int>> seqList(n, vector<int>());
+    int array[q][3];
+    int x = 0;
+    int y = 0;
+    for(int m = 0; m < q; ++m) {
+        for(int p = 0; p < 3; ++p) {
+            cin >> array[m][p];
+        }
+    }
+    for(int m = 0; m < q; ++m) {
+        x = array[m][1];
+        y = array[m][2];
+        if(array[m][0] == 1) {
+            seqList[(x^lastAnswer) % n].push_back(y);
+        }
+        else {
+            lastAnswer = seqList[(x^lastAnswer) % n][y % n];
+            cout << lastAnswer << endl;
+        }
+    }
+    
     return 0;
 }
