2d6997020587e113b15e164e1e63a618e07e5e12
Guangchu Shi <rollschild@gmail.com>
2018-02-05 17:00:55 -0500

    Update WordBreak_II.cpp

--- WordBreak_II.cpp
+++ WordBreak_II.cpp
@@ -2,23 +2,26 @@ class Solution {
 public:
     vector<string> wordBreak(string s, vector<string>& wordDict) {
         vector<string> result;
+        vector<bool> used(wordDict.size(), true);
         string temp = "";
         int leftpos = 0;
         bool initial = true;
         int initial_length = 1;
         int rightpos = leftpos + initial_length - 1;
         while(rightpos < s.length()) {
-            // if(leftpos == s.length-1) break;
-            // int rightpos = leftpos+initial_length-1;
             string sub = s.substr(leftpos, rightpos-leftpos+1);
-            if(find(wordDict.begin(), wordDict.end(), sub) != wordDict.end()) {
+            auto itr = find(wordDict.begin(), wordDict.end(), sub);
+            int distance = itr - wordDict.begin();
+            vector<bool>::iterator itr_used = used.begin() + distance;
+            if(itr != wordDict.end() && *itr_used) {
+                *itr_used = false;
                 if(initial) {
                     // the first match
                     initial_length = rightpos - leftpos + 1;
                     initial = false;
                 }
                 // string occurs in the vector/dictionary
-                if(rightpos == s.length() - 1) {
+                if(rightpos == (s.length() - 1)) {
                     // end of string
                     // need to restart
                     temp += sub;
@@ -28,6 +31,7 @@ public:
                     ++initial_length;
                     rightpos = leftpos + initial_length - 1;
                     temp = "";
+                    used.assign(used.size(), true);
                 }
                 else {
                     temp += sub;
@@ -37,7 +41,7 @@ public:
                 }
             }
             // now if rightpos not reaching end of string
-            else if(rightpos < s.length()-1) {
+            else if(rightpos < (s.length()-1)) {
                 ++rightpos;
             }
             // if rightpos is already end of string
@@ -47,6 +51,7 @@ public:
                 leftpos = 0;
                 ++initial_length;
                 rightpos = leftpos + initial_length - 1;
+                used.assign(used.size(), true);
             }
         }
         return result;
